f6ed20297797661c110c166ef4a0371c9ad673e2
DarkRedCat <85447298+DarkRedCat@users.noreply.github.com>
2023-09-14 17:08:07 +0300

    74

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('config');
-const chalk = require('chalk');
-const cors = require('cors');
-const path = require('path');
-const initDatabase = require('./startUp/initDatabase');
-const routes = require('./routes');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const config = require('config');
+// const chalk = require('chalk');
+// const cors = require('cors');
+// const path = require('path');
+// const initDatabase = require('./startUp/initDatabase');
+// const routes = require('./routes');
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
 
-app.use('/api', routes);
+// app.use('/api', routes);
 
-const PORT = config.get('port') ?? 8080;
+// const PORT = config.get('port') ?? 8080;
 
 async function start() {
   try {
